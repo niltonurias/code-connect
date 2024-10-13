452304fc694f43b240df3bb56e16bac74ea5f21f
Nilton Urias da Cruz Junior <niltonurias@gmail.com>
2024-10-13 02:06:10 -0300

    changing logger to log only in console

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,10 @@
 import { createLogger, format, transports } from 'winston';
 
 const logger = createLogger({
-  level: 'info',
+  level: 'error',
   format: format.json(),
   transports: [
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/code-connect.log', level: 'info' }),
+    new transports.Console()
   ],
 });
 
