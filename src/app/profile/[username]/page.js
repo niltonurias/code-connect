8481d5243d11d23e0ad6d3bb7bd2b8948971b873
Nilton Urias da Cruz Junior <niltonurias@gmail.com>
2024-10-22 00:18:54 -0300

    feature: creating profile page

--- src/app/profile/[username]/page.js
+++ src/app/profile/[username]/page.js
@@ -1,7 +1,9 @@
 import { redirect } from 'next/navigation';
+import Image from 'next/image';
 import db from '../../../../prisma/db';
 import styles from './page.module.css';
 import logger from '@/logger';
+import { Button } from '@/components/Button';
 
 async function findByUsername(username) {
     try {
@@ -24,11 +26,34 @@ async function findByUsername(username) {
       }
 }
 
-const PageProfile = ({ params }) => {
-    const user = findByUsername(params.username);
+const PageProfile = async ({ params }) => {
+    const user = await findByUsername(params.username);
     return (
-        <>
-        </>
+        <main className={styles.main}>
+            <div className={styles.profile}>
+                <figure className={styles.avatar}>
+                    <Image src={user.avatar} fill alt={`Avatar do usuário ${user.username}`} />
+                </figure>
+                <div className={styles.userContainer}>
+                    <div className={styles.userBasicInfo}>
+                        @{user.username}
+                        <Button>Seguir</Button>
+                    </div>
+                    <div className={styles.userInfo}>
+                        <div>{user.name}</div>
+                        <div className={styles.biography}>{user.biography}</div>
+                        <div>
+                            <p><span>{user.projects?.length ?? 0}</span> Projetos</p>
+                            <p><span>{user.followers?.length ?? 0}</span> Conexões</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.projects}>
+
+            </div>
+        </main>
     );
 }
 
